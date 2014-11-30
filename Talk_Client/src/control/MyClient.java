ff63c55263545d95aa560932b93378e7634d9a22
yeah2014 <18814092578@139.com>
2014-11-30 23:12:22 +0800

    客户端开启要输入IP

--- Talk_Client/src/control/MyClient.java
+++ Talk_Client/src/control/MyClient.java
@@ -1,11 +1,14 @@
 package control;
 
 import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
 import view.*;
 
 public class MyClient {
 
-	static String ip = "192.168.0.101";//JOptionPane.showInputDialog(null,"请输入连接的IP地址","connect",JOptionPane.OK_OPTION);
+	static String ip =JOptionPane.showInputDialog(null,"请输入连接的IP地址","connect",JOptionPane.OK_OPTION);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MyClient();
