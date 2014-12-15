8c4ed779509c7bb3a4159b197587a2d98927763b
yeah2014 <18814092578@139.com>
2014-12-15 17:03:39 +0800

    数据结构课程设计结题

--- Talk_Client/src/control/MyClient.java
+++ Talk_Client/src/control/MyClient.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import view.*;
 
 public class MyClient {
-	static String ip ="127.0.0.1";
+	static String ip ="120.24.225.226";
 //	static String ip =JOptionPane.showInputDialog(null,"请输入连接的IP地址","connect",JOptionPane.OK_OPTION);
 	public static void main(String[] args) {
 		new MyClient();
